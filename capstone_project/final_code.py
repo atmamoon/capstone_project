710384177c6ab0ace67e05049946ad4c342d2292
mamoon <sheikh.mamoon.mondal@gmail.com>
2020-08-11 18:24:03 +0530

    successfully tested the given configuration new configuration is remaining

--- capstone_project/final_code.py
+++ capstone_project/final_code.py
@@ -6,6 +6,14 @@ import milestone1 as m1
 import milestone2 as m2
 import milestone3 as m3
 
+
+Kp=2
+Ki=1
+delta_t=0.01
+speed_limit=[-30,30]
+Xerr_integral=[0,0,0,0,0,0]
+U=[0,0,0,0,0,0,0,0,0]
+
 def rearrange_to_se3(T):
     mat=[[T[i],T[i+1],T[i+2]] for i in range(0,9,3)]
     mat=np.array(mat)
@@ -30,38 +38,37 @@ def q_list(L):
     return q_chassis
 
 
-def Theta_list(L):
+def Theta_list(L,u=U):
     #joint_limits=[[-2.932,2.932],[-9999,9999],[-2.620,2.179],[-0.121,1.780],[-9999,9999]]
-    joint_limits=[[-9999,9999],[-9999,9999],[-2.163,2.179],[-1.78,1.780],[-9999,9999]]
+    #joint_limits=[[-9999,9999],[-2.5,2.67],[-2.163,2.179],[-1,1.780],[-9999,9999]]
     #joint_limits=[[-9999,9999],[-9999,9999],[-9999,9999],[-9999,9999],[-9999,9999]]
     #joint_limits=[[-2.932,2.932],[-1.117,1.553],[-2.620,2.530],[-1.780,1.780],[-9999,9999]]
 
     theta=np.zeros(5)
     for i in range(3,8):
-        if L[i]>joint_limits[i-3][0] and L[i]<joint_limits[i-3][1]:
+        '''if L[i]>joint_limits[i-3][0] and L[i]<joint_limits[i-3][1]:# and (L[i]+u[i+1]*delta_t)>joint_limits[i-3][0]\
+        #and (L[i]+u[i+1]*delta_t)<joint_limits[i-3][1]:
             theta[i-3]=L[i]
         else:
-            theta[i-3]=0
+            theta[i-3]=0'''
+        theta[i-3]=L[i]
     return theta
 
 
 
 final_configuration=[]
-initial_configuration=[0,-0.210,0,0,0.714,-2.054,1.426,0,0,0,0,0,0]
+#initial_configuration=[0,-0.210,0,0,0.714,-2.054,1.426,0,0,0,0,0,0]
+initial_configuration=[0.6,-0.210,0,0,0,-0,0,0,0,0,0,0,0]
 final_configuration.append(initial_configuration)
 
 q=q_list(final_configuration[-1])
 Tsb=np.array([[np.cos(q[0]),-np.sin(q[0]),0,q[1]],[np.sin(q[0]),np.cos(q[0]),0,q[2]],[0,0,1,0.0963],[0,0,0,1]])
 T0e=mr.FKinBody(dt.M0e,dt.B_list,Theta_list(final_configuration[-1]))
-Tse_current=np.dot(Tsb,np.dot(dt.Tb0,T0e))
-#Tse_current=np.array([[0,0,1,0],[0,1,0,0],[-1,0,0,0.5],[0,0,0,1]])
+#Tse_current=np.dot(Tsb,np.dot(dt.Tb0,T0e))
+Tse_current=np.array([[0,0,1,0],[0,1,0,0],[-1,0,0,0.5],[0,0,0,1]])
 RT=m2.TrajectoryGenerator(Tse_current,dt.Tsci,dt.Tsc_goal,dt.Tcef,dt.Tce_stand)
 N,m=np.shape(RT)
-Kp=0.15
-Ki=0.0
-delta_t=0.01
-speed_limit=[-300,301]
-Xerr_integral=[0,0,0,0,0,0]
+
 
 for i in range(N-1):
 
@@ -71,7 +78,7 @@ for i in range(N-1):
     Tse_current=np.dot(Tsb,np.dot(dt.Tb0,T0e))
     Xd=rearrange_to_se3(RT[i])
     Xd_next=rearrange_to_se3(RT[i+1])
-    U,Xerr_integral=m3.FeedbackControl(Tse_current,Xd,Xd_next,Kp,Ki,delta_t,Theta_list(final_configuration[-1]),Xerr_integral)
+    U,Xerr_integral=m3.FeedbackControl(Tse_current,Xd,Xd_next,Kp,Ki,delta_t,Theta_list(final_configuration[-1],controls_rearrange(U)),Xerr_integral)
     new_configuration=m1.NextState(final_configuration[-1],controls_rearrange(U),delta_t,speed_limit)
     if i==N-2:
         print(Xd_next)
--- capstone_project/milestone3.py
+++ capstone_project/milestone3.py
@@ -2,8 +2,25 @@ import numpy as np
 import core as mr
 import data as dt
 
+def test_joint_limits(L,u,delta_t):
+    #joint_limits=[[-2.932,2.932],[-9999,9999],[-2.620,2.179],[-0.121,1.780],[-9999,9999]]
+    #joint_limits=[[-9999,9999],[-2.5,2.67],[-2.163,2.179],[-1,1.780],[-9999,9999]]
+    #joint_limits=[[-9999,9999],[-9999,9999],[-9999,9999],[-9999,9999],[-9999,9999]]
+    joint_limits=[[-9999,9999],[-9999,9999],[-2.620,2.530],[-1.780,1.780],[-9999,9999]]#for best
+    #joint_limits=[[-2.932,2.932],[-1.117,1.553],[-2.620,2.530],[-1.780,1.780],[-9999,9999]]
+    success=True
+    theta=np.zeros(5)
+    for i in range(5):
+        if (L[i]+u[i+4]*delta_t)>joint_limits[i][0]\
+                            and (L[i]+u[i+4]*delta_t)<joint_limits[i][1]:
+            theta[i]=L[i]
+        else:
+            theta[i-3]=0
+            success=False
+    return [success,theta]
+
 def FeedbackControl(X,Xd,Xd_next,Kp,Ki,delta_t,theta,Xerr_intg=[]):
-    tolerance=1e-4
+    tolerance=1e-2
     #Xerr_intg=np.zeros((4,4))
     if len(Xerr_intg)==0:
         Xerr_intg=np.zeros(6)
@@ -14,14 +31,32 @@ def FeedbackControl(X,Xd,Xd_next,Kp,Ki,delta_t,theta,Xerr_intg=[]):
     #+mr.se3ToVec(np.dot(Kp,X_err))+mr.se3ToVec(np.dot(Ki,Xerr_intg))
     V=np.dot(mr.Adjoint(np.dot(mr.TransInv(X),Xd)),Vd)\
     +Kp*X_err+Ki*Xerr_intg
-    Jarm=mr.JacobianBody(dt.B_list,theta)
+    '''Jarm=mr.JacobianBody(dt.B_list,theta)
     Jbase=np.dot(mr.Adjoint(np.dot(mr.TransInv(mr.FKinBody(dt.M0e,dt.B_list,theta))\
     ,mr.TransInv(dt.Tb0))),dt.F)
     J=np.append(Jbase,Jarm,axis=1)
-    J[abs(J)<tolerance]=0
+    J[abs(J)<tolerance]=0'''
     #V=np.transpose(V)
     #V[abs(V)<tolerance]=0.0
-    u=np.dot(np.linalg.pinv(J),V)
+    #evaluate_joints_limits=True
+    while True:
+        Jarm=mr.JacobianBody(dt.B_list,theta)
+        Jbase=np.dot(mr.Adjoint(np.dot(mr.TransInv(mr.FKinBody(dt.M0e,dt.B_list,theta))\
+        ,mr.TransInv(dt.Tb0))),dt.F)
+        J=np.append(Jbase,Jarm,axis=1)
+        J[abs(J)<tolerance]=0
+        J_inv=np.linalg.pinv(J)
+        u=np.dot(J_inv,V)
+        J_inv=np.linalg.pinv(J)
+        evaluate_joints_limits,theta=test_joint_limits(theta,u,delta_t)
+        if evaluate_joints_limits:
+            break;
+        else:
+            continue
+
+    #J_inv=np.linalg.pinv(J)
+    #J_inv[abs(J_inv)>tolerance]=0
+    #u=np.dot(J_inv,V)
     #u[abs(u)<tolerance]=0
     if __name__=="__main__":
         print("Jacobian",J)
